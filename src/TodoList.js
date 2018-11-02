9709edd0a3ebcc317f0dbaffd94bb694fc6d2664
Andrei Vasilev <amlk158@gmail.com>
2018-11-02 11:42:55 +0200

    added some func

--- src/TodoList.js
+++ src/TodoList.js
@@ -19,20 +19,27 @@ export default class TodoList extends React.Component {
   };
 
   toggleComplete = id => {
-    this.setState(state => {
+    console.log(id)
+    this.setState(state => ({
       todos: state.todos.map(todo => {
-        if (todo.id = id) {
+        if (todo.id === id) {
+          // suppose to update
           return {
-              ...todo,
-            complete: true
-          }
+            ...todo,
+            complete: !todo.complete
+          };
         } else {
-          return todo
+          return todo;
         }
       })
+    }));
+  };
+
+  changeDisplayFilter = filter => {
+    this.setState({
+      todoToShow: filter
     })
   }
-
 
   handleToDelete = id => {
     this.setState(state => {
@@ -42,8 +49,34 @@ export default class TodoList extends React.Component {
 
 
   render() {
+
+    let todo = []
+
+    if (this.state.todoToShow === 'all') {
+      todo = this.state.todos
+    } else if (this.state.todoToShow === 'complete') {
+      todo = this.state.todos.filter(todo => todo.complete)
+    } else if (this.state.todoToShow === 'active') {
+      todo = this.state.todos.filter(todo => !todo.complete )
+    }
+  
+
+
     return(
-        <TodoForm onSumbit={this.addTodo}/>
+      <div>
+      <TodoForm onSubmit={this.addTodo}/>
+        <button onClick={() => this.changeDisplayFilter("all")}>All</button>
+        <button onClick={() => this.changeDisplayFilter("active")} >Active</button>
+        <button onClick={() => this.changeDisplayFilter("complete")}>Complete</button>
+
+        <div>
+          <ul>
+            {todo.map(note => (
+              <li onClick={() => this.toggleComplete(note.id)}>{note.text}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     )
   }
 
